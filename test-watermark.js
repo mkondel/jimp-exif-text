dea74b5386b1e0f602214be3b59f6202d1f25b20
max <mkondel@gmail.com>
2019-05-21 19:29:01 -0400

    added both broken and fixed code/images as examples

--- test-watermark.js
+++ test-watermark.js
@@ -6,18 +6,23 @@ const processImage = async () => {
   console.log(`processImage start`)
 
   const fileIn = fs.readFileSync('input.jpg')
-  const {buffer} = await jo.rotate(fileIn, {quality: 30})
-
-  const image = await Jimp.read(buffer)
-
-  const { width, height } = image.bitmap
-  const { imageSize, tags: {ExifImageWidth, ExifImageHeight, Orientation}  } = image._exif
-  const debugData = { bitmap: {width, height}, _exif: {imageSize, tags: {ExifImageWidth, ExifImageHeight, Orientation}} }
-  console.log(JSON.stringify(debugData, null, 1))
-
+  const image = await Jimp.read(fileIn)
   await image.write('output.jpg')
 
   console.log(`processImage end`)
 }
 
+const processImageFixed = async () => {
+  console.log(`processImageFixed start`)
+
+  const fileIn = fs.readFileSync('input.jpg')
+  const {buffer} = await jo.rotate(fileIn, {quality: 30})
+  const image = await Jimp.read(buffer)
+  await image.write('output-fixed.jpg')
+
+  console.log(`processImageFixed end`)
+}
+
 processImage()
+
+processImageFixed()
