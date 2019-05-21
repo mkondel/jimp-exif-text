928853ef326a0932014fa68ae24c8e54a17516bd
max <mkondel@gmail.com>
2019-05-21 19:13:42 -0400

    a fix to the EXIF orientation problem

--- test-watermark.js
+++ test-watermark.js
@@ -1,15 +1,22 @@
 const Jimp = require('jimp')
 const fs = require('fs')
+const jo = require('jpeg-autorotate')
 
 const processImage = async () => {
   console.log(`processImage start`)
+
   const fileIn = fs.readFileSync('input.jpg')
-  const image = await Jimp.read(fileIn)
+  const {buffer} = await jo.rotate(fileIn, {quality: 30})
+
+  const image = await Jimp.read(buffer)
+
   const { width, height } = image.bitmap
   const { imageSize, tags: {ExifImageWidth, ExifImageHeight, Orientation}  } = image._exif
   const debugData = { bitmap: {width, height}, _exif: {imageSize, tags: {ExifImageWidth, ExifImageHeight, Orientation}} }
   console.log(JSON.stringify(debugData, null, 1))
+
   await image.write('output.jpg')
+
   console.log(`processImage end`)
 }
 
